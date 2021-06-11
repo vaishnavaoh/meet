99ac61f3ca76ec58f94cdd9942617c37bdd55bc8
vaishnavaohio <76029991+vaishnavaohio@users.noreply.github.com>
2021-06-11 17:40:00 -0400

    Task 4.6 Edits
    If a commit is made changes to task 4.6 were made.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-
+import * as atatus from 'atatus-spa';
+atatus.config('3adb076fb2c04862aa6e9516a7d3ff55').install();
 ReactDOM.render(
   <React.StrictMode>
     <App />
