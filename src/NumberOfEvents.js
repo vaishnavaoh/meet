f943fd0a7fb020e7829e5cd71f0478f6bd1a7648
vaishnavaohio <76029991+vaishnavaohio@users.noreply.github.com>
2021-07-03 12:24:23 -0400

    Task 4.7 Edits

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
+import {ErrorAlert} from './Alert';
 
 class NumberOfEvents extends Component {
     constructor(props) {
         super(props);
         this.state = {
             eventListSize: props.number,
+            errorText: ''
         }
     }
 
     handleInputChange = (event) => {
         const number = event.target.value
-        this.setState({
-            eventListSize: number
-        });
+        if (number <= 0) {
+            this.setState({
+                eventListSize: number,
+                errorText: 'Please select a valid number.'
+            });
+        } else {
+            this.setState({
+                eventListSize: number,
+                errorText: ''
+            });
+        }
         this.props.updateListSize(number);
     }
 
     render() {
         return <div className='NumberOfEvents'>
+            <ErrorAlert text={this.state.errorText} />
             <input 
                 type="number"
                 className="number"
