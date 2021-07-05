69e65c42f13089a63b99681d14c13f265aa4bb59
vaishnavaohio <76029991+vaishnavaohio@users.noreply.github.com>
2021-07-04 23:45:05 -0400

    Task 4.8

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,11 +1,11 @@
-const { google } = require('googleapis');
+const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
-const calendar = google.calendar('v3');
+const calendar = google.calendar("v3");
 /**
  * SCOPES allows you to set access levels; this is set to readonly for now because you don't have access rights to
  * update the calendar yourself. For more info, check out the SCOPES documentation at this link: https://developers.google.com/identity/protocols/oauth2/scopes
  */
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
 /**
  * Credentials are those values required to get access to your calendar. If you see “process.env” this means
@@ -16,13 +16,13 @@ const credentials = {
   project_id: process.env.PROJECT_ID,
   client_secret: process.env.CLIENT_SECRET,
   calendar_id: process.env.CALENDAR_ID,
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  redirect_uris: ['https://vaishnavaohio.github.io/meet/'],
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  redirect_uris: ["https://vaishnavaoh.github.io/meet/"],
   javascript_origins: [
-    'https://vaishnavaohio.github.io',
-    'http://localhost:3000',
+    "https://vaishnavaoh.github.io",
+    "http://localhost:3000",
   ],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
@@ -32,21 +32,20 @@ const oAuth2Client = new google.auth.OAuth2(
   redirect_uris[0]
 );
 
-
- module.exports.getAuthURL = async () => {
+module.exports.getAuthURL = async () => {
   const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: "offline",
-      scope: SCOPES,
+    access_type: "offline",
+    scope: SCOPES,
   });
 
   return {
-      statusCode: 200,
-      headers: {
-          'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-          authUrl: authUrl,
-      }),
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      authUrl: authUrl,
+    }),
   };
 };
 
@@ -71,7 +70,7 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
+          "Access-Control-Allow-Origin": "*",
         },
         body: JSON.stringify(token),
       };
@@ -91,27 +90,30 @@ module.exports.getCalendarEvents = async (event) => {
     client_secret,
     redirect_uris[0]
   );
-    const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
-    oAuth2Client.setCredentials({ access_token });
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
+  oAuth2Client.setCredentials({ access_token });
 
-    return new Promise((resolve, reject)=>{
-      calendar.events.list(
-        {
-          calendarId: calendar_id,
-          auth: oAuth2Client,
-          timeMin: new Date().toISOString(),
-          singleEvents: true,
-          orderBy: "startTime",
-        },
-        (error, response) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(response);
-          }
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
         }
-      );
-    }).then((results) => {
+      }
+    );
+  })
+    .then((results) => {
       // Respond with OAuth token
       return {
         statusCode: 200,
